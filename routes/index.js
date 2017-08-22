16cf3a78657d5c25bc5f3d42d63e8fc941bbe88e
wohshon <wohshon@gmail.com>
2017-08-22 12:08:44 +0800

    added rest service

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,10 @@ router.get('/', function(req, res, next) {
   res.sendfile("views/index.html");
 });
 
+router.get('/rest/hello/:name', function(req, res, next) {
+  //res.send('{"response": "hello "+req.params.name }');
+  res.send({ response: req.params.name});
+});
+
 
 module.exports = router;
